d1f6eb4680451a75329734f1fb81aa84be573de4
knowckx <knowckx@foxmail.com>
2026-09-09 15:36:03 +0800

    Initial commit from template

---
+++ ai.doc/1-new_repo.md
@@ -0,0 +1,32 @@
+# 快速初始化新项目
+
+#### 克隆模板
+把 dayline 这个仓库当模板, 且不带原仓库的 Git 历史
+```bash
+pnpm dlx degit Knowckx/dayline yourAppName
+```
+
+
+#### 初始化模板
+2. 上层的 pnpm-workspace.yaml 加上新项目
+3. pnpm install
+4. 改一下端口 `vite.config.ts`  preview.port
+
+#### git
+
+```bash
+# 初始化新的 git 仓库
+git init
+git add .
+git commit -m "Initial commit from template"
+# 然后 点击vscode的发布按钮 可以直接推私人库
+```
+
+# 启动
+
+```bash
+pnpm dev
+
+# 预览生产构建
+pnpm pwa
+```
---
+++ vite.config.ts
@@ -0,0 +1,67 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+    plugins: [
+        svelte(),
+        tailwindcss(),
+        mkcert(),
+        VitePWA({
+            registerType: 'prompt',
+            includeAssets: [
+                'apple-touch-icon.png'
+            ],
+            manifest: {
+                name: 'dayline',
+                short_name: 'dayline',
+                description: 'dayline',
+                display: 'standalone',
+                start_url: '/',
+                scope: '/',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            workbox: {
+                navigateFallback: '/index.html',
+                cleanupOutdatedCaches: true,
+                clientsClaim: false,
+                skipWaiting: false,
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest,woff2}']
+            }
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(process.cwd(), 'src')
+        }
+    },
+    // ✅ 必须 - 禁用客户端预构建本地包的缓存
+    optimizeDeps: {
+        exclude: ['@knowckx/infa-s5']
+    },
+    server: { // 开发地址
+        https: {},
+        port: 23001,
+        host: true,
+    },
+    preview: { // 生产的pwa地址
+        port: 33001,
+        host: true
+    },
+});
